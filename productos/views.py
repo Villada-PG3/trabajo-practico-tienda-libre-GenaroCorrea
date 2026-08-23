0c50a936249ca7b6a5a9e4b0a51d992fde0edfb4
Genaro <g.correa@itsv.edu.ar>
2026-08-23 17:38:52 +0000

    feat: primera vista y template para home y acerca-de-mi

--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'productos/home.html')
+
+def acerca_de(request):
+    return render(request, 'productos/acerca-de-mi.html')
